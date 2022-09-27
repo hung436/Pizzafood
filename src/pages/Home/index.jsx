a32dc1fdcca47e3d745aff6880b8f834b3374b34
DoanVanThang1 <doanvanthang172001@gmail.com>
2022-09-27 09:06:55 +0700

    sua tesst chu moinhat

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="h-[67px] bg-[#0b1a33] flex justify-center font-semibold text-xs w-full">
         <ul className="flex text-white items-center justify-start space-x-5 md:w-[1140px] overflow-x-auto whitespace-nowrap">
           <li className="underline active w-fit">NHÀ SÁNG TẠO NỘI DUNG</li>
-          <li className="underline">MỚI NHẤT</li>
+          <li className="underline">MỚI NHẤ</li>
           <li className="underline">SERIES</li>
           <li className="underline">EDITOR' CHOICE</li>
           <li className="underline">TRENDING</li>
