b2be78c5fb6395147a823959beeabe8555a850ab
hung436 <ttldhung@gmail.com>
2022-10-05 11:24:44 +0700

    fix slide

--- src/pages/Home/component/Slide/Slide.jsx
+++ src/pages/Home/component/Slide/Slide.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { Carousel } from "flowbite-react";
 function Slide({ ImageLink }) {
   return (
-    <div className='h-56 sm:h-64 xl:h-80 2xl:h-96'>
+    <div className='md:h-56 h-40  xl:h-80 2xl:h-96'>
       <Carousel>
         {ImageLink.map((value, index) => (
-          <img key={index} src={value} alt='...' />
+          <img
+            className='md:h-[400px] h-40'
+            key={index}
+            src={value}
+            alt='...'
+          />
         ))}
       </Carousel>
     </div>
