730913d028199f6f5e13f0196329adc0ebe3a48c
PhanNhat87 <phanminhnhat87@gmail.com>
2022-10-11 07:31:32 +0700

    [feat] notfound&admin

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,7 @@ import slide3 from "../../assets/img/slide3.jpg";
 const ImageLink = [slide1, slide2, slide3];
 const Home = () => {
   return (
-    <div className=''>
+    <div className="">
       <div className='h-[67px] bg-[#0b1a33] flex justify-center font-semibold text-xs w-full'>
         <ul className='flex text-white items-center justify-start space-x-5 md:w-[1140px] overflow-x-auto whitespace-nowrap'>
           <li className='underline active w-fit'>NHÀ SÁNG TẠO NỘI DUNG</li>
--- src/pages/Notfound/index.jsx
+++ src/pages/Notfound/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import pageFound from "./pageFound.jpg";
+import "./indexs.css";
 
 const NotFound = () => {
-  return <div>404 NotFound</div>;
+  return ( 
+        <div className="pageNotFound">   
+        <div className='pnfound'>
+            <h1>Oops!</h1>
+            <h2>404 - PAGE NOT FOUND</h2>
+            <p>The page you are looking for might have been removed <br/> had its name changed or is temporarily unavailable</p>
+        </div>        
+            <img src={pageFound} alt={"pageFound"} height="50" weight="500"/>
+            <div className='goto'>
+              <a href="/">GO TO HOMEPAGE</a>
+            </div>
+        </div>  
+  )
 };
 
 export default NotFound;
