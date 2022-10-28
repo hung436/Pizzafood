e6aec54124ae1a9929165500000e33a2f42e2b4e
hung436 <ttldhung@gmail.com>
2022-10-28 20:39:39 +0700

    [feat] router add api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import Header from "./components/Header";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/App.scss";
-import { Routes, Route, Outlet, Navigate, useRoutes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+import { dispatch } from "./app/Store/store";
 
-import Footer from "./components/Footer/Footer.jsx";
-import Menu from "./pages/Menu/menu.jsx";
-import NotFound from "./pages/Notfound";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
 import "./styles/globals.css";
-import React, { Suspense } from "react";
-import { useSelector } from "react-redux";
+import React, { Suspense, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Loading from "./components/Loading";
 import routes from "./routes/route";
+// import { dispatch } from "./app/Store/store";
+import { getInfor } from "./app/Reducer/authSlice";
 
 function App() {
   const { userInfo } = useSelector((state) => state.user);
-  console.log(userInfo);
+  console.log("userInfo", userInfo);
   // const PrivateRoute = ({ children }) => {
   //   return userInfo && userInfo.RoleId === 2 ? (
   //     children
@@ -24,6 +22,14 @@ function App() {
   //     <Navigate to='/login' />
   //   );
   // };
+  // const dispatch = useDispatch();
+  useEffect(() => {
+    try {
+      dispatch(getInfor());
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
   const routing = useRoutes(routes(userInfo && userInfo.RoleId === "admin"));
   return (
     <div className='App'>
