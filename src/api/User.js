6a1d9c0ba10ac365f99e1d9a237e7bda2ad6867e
hung436 <ttldhung@gmail.com>
2022-11-24 13:49:33 +0700

    [fix] add antd

---
+++ src/api/User.js
@@ -0,0 +1,24 @@
+import instance from "./axiosConfig";
+
+const getUserList = () => {
+  return instance.get("/users");
+};
+const getUserById = (id) => {
+  return instance.get(`/users/findbyid/${id}`);
+};
+const createUser = (data) => {
+  return instance.post("/product/create", data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
+const updateUser = (data) => {
+  return instance.post("/users/update", data);
+};
+
+const userApi = {
+  getUserList,
+  createUser,
+  updateUser,
+  getUserById,
+};
+export default userApi;
