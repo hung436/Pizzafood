91d4d8f8a4ea271bfef141ef2bc052cc8e18944b
hung436 <ttldhung@gmail.com>
2022-11-29 08:34:40 +0700

    [fix ]

--- src/pages/Home/component/ProductCategory/index.jsx
+++ src/pages/Home/component/ProductCategory/index.jsx
@@ -26,7 +26,7 @@ function ProductCategory() {
         <span className={css.headingProduct}>Mì Ý</span>
       </div>
 
-      <ListProduct data={products.filter(value)} />
+      <ListProduct data={products} />
 
       <div className={css.heading}>
         <span className={css.headingProduct}>Đồ uống</span>
